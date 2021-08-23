2696ac4e8395e52eeaaad05b4ac27ad1afa2763b
Aryan Vats <aryan.vats68@gmail.com>
2021-08-24 01:08:07 +0530

    Adding visibility in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast= (longitude,latitude,callback)=>{
         }else if(body.error){
             callback("Invald coordinates",undefined)
         }else{
-            callback(undefined,"Overcast: "+body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees.Also, the city has a visibility of '+body.current.visibility.visibility)
+            callback(undefined,"Overcast: "+body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees.Also, the city has a visibility of '+body.current.visibility+" metres.")
         }
     })
 }
